8f1f19ae45b3b30ac7fe4eb257daaa70c2f51de3
paninapress <NPacifico25@gmail.com>
2014-01-28 19:37:35 -0800

    product views initial set up

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,19 @@ Store::Application.routes.draw do
   #sites controller
   root 'sites#index'
 
-
   #products controller
 
     get '/products', to: 'products#index', as: 'products'
+    
+    get '/products/new', to: 'products#new', as: :new_product
 
+    get '/products/:id', to: 'products#show', as: :product
 
+    get '/products/:id/edit', to: 'products#edit', as: :edit_product
+
+    post '/products', to: 'products#create'
+
+    patch '/products/:id', to: 'products#update'
 
 
   #categories controller
