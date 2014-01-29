c25e75cf2b3fa6dc10020c6c03de7d5ad0cfa598
paninapress <NPacifico25@gmail.com>
2014-01-28 20:26:36 -0800

    initial category view set up

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,18 @@
 class CategoriesController < ApplicationController
+
+	def index
+		@categories = Category.all
+	end
+
+	def new
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+	end
 end
