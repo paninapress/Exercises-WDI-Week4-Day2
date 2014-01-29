26627e0d6ec1cb0296b5ec56603660d705c60f28
paninapress <NPacifico25@gmail.com>
2014-01-29 09:41:00 -0800

    got update working

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,8 +10,10 @@ class CategoriesController < ApplicationController
 
 	def create
 		category = Category.create(name: params["category"]["name"])
-		params["category"]["product_ids"].each { |x| Association.create(product_id: x, category_id: category_id) if x != "" }
-		#goes into the params hash into the product_ids hash related by "Association"
+		params["category"]["product_ids"].each { 
+			|x| Association.create(product_id: x, category_id: category_id) if x != "" }
+		#goes into the params hash into the product_ids hash 
+		#related by "Association"
 
 		redirect_to category
 	end
@@ -29,7 +31,7 @@ class CategoriesController < ApplicationController
 		category = Category.find(params[:id])
 		updated_info = params.require(:category).permit(:name)
 		category.update_attributes(updated_info)
-		product.product_ids = params["product"]["product_ids"]
+		category.product_ids = params["category"]["product_ids"]
 		redirect_to category
 	end
 end
