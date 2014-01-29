7d5d105e1eeb78076fd2790acb0553784d19a396
paninapress <NPacifico25@gmail.com>
2014-01-28 21:37:48 -0800

    all category views working

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,14 +5,27 @@ class CategoriesController < ApplicationController
 	end
 
 	def new
+		@category = Category.new
+	end
+
+	def create
+		category = Category.create(name: params["category"]["name"])
+		redirect_to category
 	end
 
 	def show
+		@category = Category.find(params[:id])
+		@products = @category.products
 	end
 
 	def edit
+		@category = Category.find(params[:id])
 	end
 
 	def update
+		category = Category.find(params[:id])
+		updated_info = params.require(:category).permit(:name)
+		category.update_attributes(updated_info)
+		redirect_to category
 	end
 end
