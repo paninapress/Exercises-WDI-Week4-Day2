a96b26583dd17e0ea44a2968552fe211d37ed173
paninapress <NPacifico25@gmail.com>
2014-01-28 23:12:48 -0800

    can create/edit w/both products/categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,6 +10,9 @@ class CategoriesController < ApplicationController
 
 	def create
 		category = Category.create(name: params["category"]["name"])
+		params["category"]["product_ids"].each { |x| Association.create(product_id: x, category_id: category_id) if x != "" }
+		#goes into the params hash into the product_ids hash related by "Association"
+
 		redirect_to category
 	end
 
@@ -26,6 +29,7 @@ class CategoriesController < ApplicationController
 		category = Category.find(params[:id])
 		updated_info = params.require(:category).permit(:name)
 		category.update_attributes(updated_info)
+		product.product_ids = params["product"]["product_ids"]
 		redirect_to category
 	end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,6 +10,7 @@ class ProductsController < ApplicationController
 	def create
 		product = Product.create(name: params["product"]["name"], price: params["product"]["price"], description: params["product"]["description"])
 		params["product"]["category_ids"].each { |x| Association.create(product_id: product.id, category_id: x) if x != "" }
+		#goes into the params hash into the category_ids hash related by "Association"
 		redirect_to product
 	end
 
@@ -26,6 +27,7 @@ class ProductsController < ApplicationController
 		product = Product.find(params[:id])
 		updated_info = params.require(:product).permit(:name, :price, :description)
 		product.update_attributes(updated_info)
+		product.category_ids = params["product"]["category_ids"]
 		redirect_to product
 	end
 end
